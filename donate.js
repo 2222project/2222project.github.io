bb10ff1f3b609948cb3655f08e28efb925293f25
Kimberley Burchett <kim.burchett@gmail.com>
2016-09-06 05:39:46 -0400

    Donate page now has dynamically updating text.  Also, removed geographical targeting for now.

--- donate.js
+++ donate.js
@@ -5,22 +5,34 @@ function numberWithCommas(x) {
     return parts.join(".");
 }
 
-function extrapolate_monthly_contribution(amount, max_year) {
+function deduct_paypal_fees(contribution) {
+  return Math.max(0, contribution * 0.983 - 0.3);
+}
+
+function project_one_time_contribution(amount) {
+  // this is fragile because it depends on the fact that we treat monthly contributions
+  // as a series of annual contributions.
+  return project_monthly_contribution(amount / 12.0, 1);
+}
+
+function project_monthly_contribution(amount, max_year) {
   var result = [];      // :: [ {balance, payout, total_payout} ]
   var balance = 0;
+  var total_contribution = 0;
   var total_payout = 0;
   for (var year=0; year <= 210; year++) {
     balance = balance * 1.05;
-    if (year < max_year) {    // assume contributions stop after 10 years
-      var contribution = (amount * 0.983 - 0.3) * 12;    // account for paypal fees
-      balance = balance + contribution;
+    if (year < max_year) {
+      balance += deduct_paypal_fees(amount) * 12;
+      total_contribution += amount * 12;
     };
     var payout = balance * 0.015;
-    total_payout = total_payout + payout;
+    total_payout += payout;
     result.push( {
       balance: balance,
       payout: payout,
       total_payout: total_payout,
+      total_contribution: total_contribution,
     } );
   }
   return result;
